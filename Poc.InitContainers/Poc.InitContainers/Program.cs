1941b76449655eb5797dcf1970ce6602658f8209
Ciobanu Cristian <cristian.ciobanu1998@outlook.com>
2022-04-08 10:35:07 +0300

    increment

--- Poc.InitContainers/Poc.InitContainers/Program.cs
+++ Poc.InitContainers/Poc.InitContainers/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 while(true)
 {
+    int i=0;
     Thread.Sleep(10000);
-    Console.WriteLine("Hello from app");
+    Console.WriteLine($"Hello from app {i}");
+    i++;
 }
